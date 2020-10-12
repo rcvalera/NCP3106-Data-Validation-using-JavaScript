57acb55d4f99217d88978e6ad34dc1ba99b53e5a
Darwin Tiqui <tiqui.darwin@ue.edu.ph>
2020-10-12 20:15:56 +0800

    New file with php

---
+++ AccountRegistration.php
@@ -0,0 +1,52 @@
+<?php
+
+$last_name = $_POST['last-name'];
+$first_name = $_POST['first-name'];
+$mid_initial = $_POST['mid-initial'];
+$studnumber = $_POST['stud-number']; 
+$year_level = $_POST['year-level'];
+$birthday = $_POST['birthday'];
+$mobnumber = $_POST['mobnumber']; 
+$email = $_POST['ue-email'];
+$username = $_POST['user-name'];
+$password = $_POST['pass']; 
+$passwordRepeat = $_POST['passwordrepeat']; 
+
+
+	if ($password == $passwordRepeat ) {
+		insertRecord($last_name,$first_name,$mid_initial,$studnumber,$year_level,$birthday,$mobnumber,$email,$username,$password);
+	}
+
+	else {
+		echo '<script>
+  				alert("Passwrod Mismatch!");
+					</script>';
+
+		echo '<script>
+				window.history.go(-1);
+					</script>';
+		}
+
+
+
+function insertRecord($last_name,$first_name,$mid_initial,$studnumber,$year_level,$birthday,$mobnumber,$email,$username,$password) {
+ try {
+ require 'openDB.php';
+     
+  $sql = "INSERT INTO databasae2 (lastName, firstName, middleInitial, studNumber, YearLevel, Birthday, Mobile, Email, Username, Password) VALUES (?,?,?,?,?,?,?,?,?,?)";
+     
+     
+  // use exec() because no results are returned 
+     $conn->prepare($sql)->execute([$last_name,$first_name,$mid_initial,$studnumber,$year_level,$birthday,$mobnumber,$email,$username,$password]);
+
+  echo '<script>
+  				alert("Congratulations, you are now registered!");
+					</script>';
+} catch(PDOException $e) {
+  echo $sql . "<br>" . $e->getMessage();
+}
+
+$conn = null;
+}
+
+?>
